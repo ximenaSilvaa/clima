9f04e74c17d0f852a3a480926f42079b950887a8
Kei300 <a01666624@tec.com>
2025-05-08 17:44:03 -0600

    bienvenida front

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import './index.css'
 import Home from './home'
 import Grupos from './grupos'
+import Bienvenida from './bienvenida'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Bienvenida />} />
+      <Route path="/login" element={<Bienvenida />} />
       <Route path="hola" element={<Grupos />} />
     </Routes>
   )
