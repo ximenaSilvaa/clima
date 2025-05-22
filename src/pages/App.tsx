56ac19098658ecce44f29482bdb3fe3e1cba96c4
ximenaSilvaa <a01785518@tec.mx>
2025-05-22 16:47:41 -0600

    Ajustes visuales en Alumno y login: diseño, íconos y modal

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,15 +5,18 @@ import Grupos from './grupos'
 import Grupo from './grupo'
 import Bienvenida from './bienvenida'
 import BuscarAlumno from './BuscarAlumno'
+import Alumno from './alumno'
+import Login from './login'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Bienvenida />} />
-      <Route path="/login" element={<Bienvenida />} />
       <Route path="/buscar" element={<BuscarAlumno />} />
       <Route path="hola" element={<Grupos />} />
       <Route path="que_hace" element={<Grupo />} />
+      <Route path="login" element={<Login />} />
+      <Route path="/alumno" element={<Alumno />} />
     </Routes>
   )
 }
