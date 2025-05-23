236701f86279449397d2128c91b7e0fca8d975b1
Kei300 <a01666624@tec.com>
2025-05-22 23:08:55 -0600

    buscar

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,12 +3,14 @@ import './index.css'
 import Home from './home'
 import Grupos from './grupos'
 import Bienvenida from './bienvenida'
+import BuscarAlumno from './BuscarAlumno'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Bienvenida />} />
       <Route path="/login" element={<Bienvenida />} />
+      <Route path="/buscar" element={<BuscarAlumno />} />
       <Route path="hola" element={<Grupos />} />
     </Routes>
   )
