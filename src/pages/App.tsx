7a9fd51e3c98960aaca1ee00d9d0f27ca1a96c0f
David-Brnb <321171197@ciencias.unam.mx>
2025-05-27 16:24:11 -0600

    add: some changes

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,11 @@
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import './index.css'
-import Home from './home'
 import Grupos from './grupos'
 import Grupo from './grupo'
-import Bienvenida from './bienvenida'
-import BuscarAlumno from './BuscarAlumno'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Bienvenida />} />
-      <Route path="/login" element={<Bienvenida />} />
-      <Route path="/buscar" element={<BuscarAlumno />} />
       <Route path="hola" element={<Grupos />} />
       <Route path="que_hace" element={<Grupo />} />
     </Routes>
