ec5d4f5678d3f61623399b32200c7ac767da8fec
David-Brnb <321171197@ciencias.unam.mx>
2025-06-05 15:42:38 -0600

    add: share info through pages

--- src/componentes/GroupCard.tsx
+++ src/componentes/GroupCard.tsx
@@ -42,7 +42,7 @@ function GroupCard({ group }: GroupCardProps) {
                 <div className='flex justify-end gap-2 mt-4'>
                     <button 
                         className="bg-white text-cyan-600 border border-cyan-400 px-4 py-2 rounded hover:bg-gray-100 transition"
-                        onClick={() => navigate('/grupo')}
+                        onClick={() => navigate('/grupo', { state: { students: group.students } })}
                     >
                         Visualizar
                     </button>
--- src/pages/grupo.tsx
+++ src/pages/grupo.tsx
@@ -1,8 +1,12 @@
 import Header from '../componentes/Header';
 import StudentCard from '../componentes/Student';
-import { students } from '../types/data';
+import { useLocation } from 'react-router-dom'
+import { StudentInformation } from '../types/types';
+// import { students } from '../types/data';
 
 function Grupo() {
+    const location = useLocation();
+    const { students } = location.state || {};
 
     return (
         <div className="h-auto w-auto bg-primary flex flex-col">
@@ -17,7 +21,7 @@ function Grupo() {
             <div className="flex flex-col items-center justify-start min-h-screen w-full py-10 px-20">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-20 px-4 w-full">
                     {
-                    students.map((student) => (
+                    students.map((student: StudentInformation) => (
                         <StudentCard student={student}/>
                     ))
                     }
